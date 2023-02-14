03138097b3bc4bd8f4dea7e42b2d9687b42f3d6a
Ayush Goyal <ayush_g@ar.iitr.ac.in>
2023-02-14 13:28:32 +0530

    Basic UI Setup

---
+++ src/components/Change.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Change() {
+  return (
+    <>
+    <button type="button" class="btn btn-primary mx-5">+</button>
+    <button type="button" class="btn btn-primary">-</button>
+
+    </>
+  )
+}
+
+export default Change
